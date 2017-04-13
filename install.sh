90e69dad04b436f0b864a287ada460ad37db5b1e
Loïc Mahieu <mahieuloic@gmail.com>
2017-04-13 16:03:17 +0200

    v0.0.9

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 REPOSITORY="git://github.com/igloo-be/igloo-build-tools.git"
-VERSION="v0.0.8"
+VERSION="v0.0.9"
 TMPDIR="/tmp/igloo-build-tools"
 INSTALL_DIR="/usr/local/bin"
 
